d73c70c91645e75e5af68a0d96bba2aa82287a7f
Sami Munir <sami.munir2001@gmail.com>
2025-04-15 15:32:37 -0400

    establishing MongoDB connection with subscription-tracker backend Express server

--- Learning/subscription-tracker/routes/subscription.routes.js
+++ Learning/subscription-tracker/routes/subscription.routes.js
@@ -6,4 +6,32 @@ subscriptionRouter.get('/', (req, res) => {
     res.send('GET all subscriptions.');
 });
 
+subscriptionRouter.get('/:id', (req, res) => {
+    res.send(`GET subscription with ID: ${req.params.id}`);
+});
+
+subscriptionRouter.post('/', (req, res) => {
+    res.send('CREATE a new subscription.');
+});
+
+subscriptionRouter.put('/:id', (req, res) => {
+    res.send(`UPDATE subscription with ID: ${req.params.id}`);
+});
+
+subscriptionRouter.delete('/:id', (req, res) => {
+    res.send(`DELETE subscription with ID: ${req.params.id}`);
+});
+
+subscriptionRouter.get('/user/:id', (req, res) => {
+    res.send(`GET all subscriptions for user with ID: ${req.params.id}`);
+});
+
+subscriptionRouter.put('/:id/cancel', (req, res) => {
+    res.send(`CANCEL subscription with ID: ${req.params.id}`);
+});
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => {
+    res.send('GET all upcoming renewals.');
+});
+
 export default subscriptionRouter;
