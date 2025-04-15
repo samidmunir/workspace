1b0744bb27ee0024be6ed94a9753c03036de9651
Sami Munir <sami.munir2001@gmail.com>
2025-04-15 01:41:17 -0400

    restarting project: career-dock

--- Projects/career-dock/backend/server.js
+++ Projects/career-dock/backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
+// app.get('/records', (req, res) => {});
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    connectDB();
+    console.log('Server is running on: http://localhost:3000');
 });
