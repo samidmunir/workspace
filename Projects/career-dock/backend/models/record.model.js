f6fe166afbaef1a29fd6f83661c5e893e382ced9
Sami Munir <sami.munir2001@gmail.com>
2025-04-16 20:26:45 -0400

    creating job app Record model

---
+++ Projects/career-dock/backend/models/record.model.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+const recordSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Job title is required'],
+        trim: true,
+        minLength: 5,
+        maxLength: 100,
+    },
+    company: {
+        type: String,
+        required: [true, 'Company name is required'],
+        trim: true,
+        minLength: 5,
+        maxLength: 50,
+    },
+    description: {
+        type: String,
+        required: [true, 'Job description is required'],
+        trim: true,
+        minLength: 20,
+        maxLength: 5000,
+    },
+    type: {
+        type: String,
+        required: [true, 'Job type is required'],
+        enum: ['full-time', 'part-time', 'contract', 'internship'],
+    },
+    location: {
+        type: String,
+        required: [true, 'Location is required'],
+        trim: true,
+        minLength: 5,
+        maxLength: 100,
+    },
+    salary: {
+        type: Number,
+        required: [true, 'Salary is required'],
+        min: 5000,
+    },
+    status: {
+        type: String,
+        required: [true, 'Status is required'],
+        enum: ['active', 'pending', 'inactive'],
+        default: 'active',
+    },
+}, {timestamps: true});
+
+const Record = mongoose.model('Record', recordSchema);
+
+export default Record;
