418b5babf77f51c3bf3cace4d177bf96123748a5
Sami Munir <sami.munir2001@gmail.com>
2025-04-20 02:01:32 -0400

    GenderCollection component complete (part of home page)

--- Learning/rabbit-ecommerce/frontend/src/pages/Home.tsx
+++ Learning/rabbit-ecommerce/frontend/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from '../components/layout/Hero.tsx';
+import GenderCollection from '../components/products/GenderCollection.tsx';
 
 const Home = () => {
     return (
         <div>
             <Hero />
+            <GenderCollection />
         </div>
     );
 };
