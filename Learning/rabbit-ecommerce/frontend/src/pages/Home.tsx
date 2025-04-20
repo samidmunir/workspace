a531be522896012c148ddbe38d2f04d42fa604df
Sami Munir <sami.munir2001@gmail.com>
2025-04-20 03:18:24 -0400

    implementing New Arrivals section for Home page

--- Learning/rabbit-ecommerce/frontend/src/pages/Home.tsx
+++ Learning/rabbit-ecommerce/frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from '../components/layout/Hero.tsx';
 import GenderCollection from '../components/products/GenderCollection.tsx';
+import NewArrivals from '../components/products/NewArrivals.tsx';
 
 const Home = () => {
     return (
         <div>
             <Hero />
             <GenderCollection />
+            <NewArrivals />
         </div>
     );
 };
