9397f4939e7ec6d927704c3e2baee0a6711b5f12
Sami Munir <sami.munir2001@gmail.com>
2025-04-23 18:59:31 -0400

    implementing first url & view (hello_world)

--- Learning/Django-blog/blog/posts/views.py
+++ Learning/Django-blog/blog/posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpRequest, HttpResponse
+
 # Create your views here.
+
+def hello_world(request: HttpResponse):
+    return HttpResponse('Hello world!')
